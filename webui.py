2e1501771f596b1f91d952409d3d31e06774fc4b
dreamtimeux <160812710+dreamtimeux@users.noreply.github.com>
2024-03-17 22:42:32 +0600

    Update webui.py

--- webui.py
+++ webui.py
@@ -605,7 +605,7 @@ with shared.gradio_root:
                                                             queue=False, show_progress=False)
 
                     with gr.Tab(label='FreeU'):
-                        freeu_enabled = gr.Checkbox(label='Enabled', value=True)
+                        freeu_enabled = gr.Checkbox(label='Enabled', value=False)
                         freeu_b1 = gr.Slider(label='B1', minimum=0, maximum=2, step=0.01, value=1.3)
                         freeu_b2 = gr.Slider(label='B2', minimum=0, maximum=2, step=0.01, value=1.4)
                         freeu_s1 = gr.Slider(label='S1', minimum=0, maximum=4, step=0.01, value=0.9)
